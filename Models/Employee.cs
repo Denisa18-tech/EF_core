9de30e6f373550353cf3baf7818bf9224e0b63cc
Denisa Khodbhaya <denisa@evisionits.com>
2026-06-02 14:24:36 +0530

    Add files via upload
    # Code-First vs Database-First
    
    ## What is Code-First?
    
    In Code-First approach, developers create C# classes first.
    
    EF Core generates database tables from those classes.
    
    ### Flow
    
    C# Classes → EF Core → Database
    
    ### Example
    
    csharp-C#
    public class Employee
    {
        public int Id { get; set; }
        public string Name { get; set; }
    }

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompanyDbApp.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? Salary { get; set; }
+}
