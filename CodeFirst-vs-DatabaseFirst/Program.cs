43df2190cf9b231f1734fd5a5d5b26ac2d4e0548
Denisa Khodbhaya <denisa@evisionits.com>
2026-06-02 14:31:00 +0530

    Add files via upload

---
+++ CodeFirst-vs-DatabaseFirst/Program.cs
@@ -0,0 +1,13 @@
+﻿using CompanyDbApp.Models;
+
+var db = new CompanyDbContext();
+
+var employees = db.Employees.ToList();
+
+foreach (var emp in employees)
+{
+    Console.WriteLine(emp.Id);
+    Console.WriteLine(emp.Name);
+    Console.WriteLine(emp.Salary);
+    Console.WriteLine("------------");
+}
